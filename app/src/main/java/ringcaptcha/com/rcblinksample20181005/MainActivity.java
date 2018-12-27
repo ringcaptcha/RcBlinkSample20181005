dac68b3f04bf69a69a28f2095f66c6a5ba7c8f74
yutaf <yutafuji2008@gmail.com>
2018-12-27 15:55:11 +0900

    Remove unused method

--- app/src/main/java/ringcaptcha/com/rcblinksample20181005/MainActivity.java
+++ app/src/main/java/ringcaptcha/com/rcblinksample20181005/MainActivity.java
@@ -63,15 +63,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    public static boolean hasPermissions(Context context, String... permissions) {
-        if (context != null && permissions != null) {
-            for (String permission : permissions) {
-                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 }
